626a06f8d294afa7b611130160b13cc9e1c3ec01
songpygit <spy_187@sina.com>
2019-04-24 16:41:37 +0800

    Initial commit

---
+++ src/test/java/com/songpy/mybatis_plus_demo/MybatisPlusDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.songpy.mybatis_plus_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybatisPlusDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
